a8b9f8b3a0f1813bbcf405202a492c6243886ae2
Oyny <oxana1404@gmail.com>
2018-12-10 11:34:07 +0200

    AddDatePiacker

--- app/src/main/java/com/dikaya/mobile/LessonLayout/LessonActivity/LoginActivity.java
+++ app/src/main/java/com/dikaya/mobile/LessonLayout/LessonActivity/LoginActivity.java
@@ -1,22 +1,83 @@
 package com.dikaya.mobile.LessonLayout.LessonActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.dikaya.mobile.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+//    @Override
+//    protected void onCreate(Bundle stage){
+//        super.onCreate(stage);
+//
+//        setContentView(R.layout.activity_login);
+//
+//        Button btn  = (Button) findViewById(R.id.btnLogin);
+//
+//        btn.setOnClickListener(view -> {
+//
+//            Intent container = new Intent(this,ProfileActivity.class);
+//
+//            EditText txtLogin = (EditText) findViewById(R.id.txtLoginLogin);
+//            EditText txtPassword = (EditText) findViewById(R.id.txtLoginPassword);
+//
+//            container.putExtra("login",txtLogin.getText());
+//            container.putExtra("password",txtPassword.getText());
+//
+//            startActivity(container);
+//        });
+//
+//        TextView lblRegistered = (TextView)findViewById(R.id.lblRegister);
+//    }
+
+    public static final String LOGIN = "LoginActivity_login";
+
+    public static final String PASSWORD = "LoginActivity_password";
+
+
+    public static final String REGIME = "LoginActivityregime";
+
+    protected void onCreate(Bundle stage){
+        super.onCreate(stage);
+
         setContentView(R.layout.activity_login);
 
+        Button btn  = (Button) findViewById(R.id.btnLogin);
 
+        btn.setOnClickListener(view -> {
+
+            Intent container = new Intent(this,ProfileActivity.class);
+
+            EditText txtLogin = (EditText) findViewById(R.id.txtLoginLogin);
+            EditText txtPassword = (EditText) findViewById(R.id.txtPass);
+
+
+            container.putExtra(LOGIN,txtLogin.getText().toString());
+            container.putExtra(PASSWORD,txtPassword.getText().toString());
+            container.putExtra(REGIME,ProfileActivity.REGIME_PROFILE);
+
+            startActivity(container);
+        });
+
+
+        TextView lblRegistered = (TextView)findViewById(R.id.lblRegister);
+        lblRegistered.setOnClickListener(view -> {
+
+            Intent container = new Intent(this,ProfileActivity.class);
+
+            container.putExtra(REGIME,ProfileActivity.REGIME_REGISTER);
+
+            startActivity(container);
+
+        });
     }
-
 }
--- app/src/main/java/com/dikaya/mobile/LessonLayout/LessonActivity/ProfileActivity.java
+++ app/src/main/java/com/dikaya/mobile/LessonLayout/LessonActivity/ProfileActivity.java
@@ -1,20 +1,101 @@
 package com.dikaya.mobile.LessonLayout.LessonActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.dikaya.mobile.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+
+
+    public static final String REGIME_REGISTER = "ProfileActivity_register_regime";
+    public static final String REGIME_PROFILE = "ProfileActivity_register_profile";
+
+    private static final int REQUEST_DATE = 1;
+
+    protected void onCreate(Bundle stage) {
+        super.onCreate(stage);
+
+        Intent container = getIntent();
+
+        Bundle extras = container.getExtras();
+
+        if (extras!=null) {
+
+            String login = extras.getString(LoginActivity.LOGIN);
+            String password = extras.getString(LoginActivity.PASSWORD);
+
+
+            setContentView(R.layout.activity_profile);
+
+            EditText txtLogin = (EditText) findViewById(R.id.txtLogin);
+            txtLogin.setText(login);
+
+            EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+            txtPassword.setText(password);
+        }
+
+// Birthday DatePiacker
+        findViewById(R.id.txtBirthday).setOnKeyListener(null);
+        ((EditText)findViewById(R.id.txtBirthday)).setCursorVisible(false);
+//        ((EditText)findViewById(R.id.txtBirthday)).setInputType(InputType.TYPE_NULL);
+        findViewById(R.id.txtBirthday).setPressed(false);
+        findViewById(R.id.txtBirthday).setClickable(true);
+
+
+        findViewById(R.id.txtBirthday).setOnClickListener(view -> {
+
+            Intent intent = new Intent(this, DatePickerControlActivity.class);
+
+//            startActivity(intent);
+
+            startActivityForResult(intent,REQUEST_DATE);
+        });
+
+//        // Country Spiner Combobox
+//        String[] countries = getResources().getStringArray(R.array.countries);
+//
+//        Spinner cmbCountrie = (Spinner)findViewById(R.id.cmbCountry);
+//        // ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.simple_spinner_item, countries);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.simple_spinner_item, countries);
+//
+//        adapter.setDropDownViewResource((R.layout.support_simple_spinner_dropdown_item));
+//        cmbCountrie.setAdapter(adapter);
+
+
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        if (requestCode == REQUEST_DATE){
+
+
+            if (resultCode ==RESULT_OK){
+
+                String birthdays =  data.getExtras().getString(DatePickerControlActivity.BIRTHDAY);
+
+                ((EditText)findViewById(R.id.txtBirthday)).setText(birthdays);
+
+            }else
+                ((EditText)findViewById(R.id.txtBirthday)).setText("");
+
+        }
+        else if (requestCode == 23453){ //"some other activy closed"
+
+
+        }
+        else
+            super.onActivityResult(requestCode,resultCode,data);
 
     }
 
